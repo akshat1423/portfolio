2cf0f1032d4fdc2855b7e63e78ffc22dffdf9c58
akshat1423 <22b0690@iitb.ac.in>
2024-12-07 10:05:00 +0530

    changes

--- src/constants/eduConstant.js
+++ src/constants/eduConstant.js
@@ -3,10 +3,28 @@ import IITBImg from "../assets/iitb.png";
 export const educationData = [
   {
     id: 1,
-    degree: "Bachelor of Technology in Computer Science and Engineering",
+    degree: "Bachelor of Technology in Engineering",
     minordegree: "Machine Learning and Data Science",
     institution: "Indian Institute of Technology Bombay",
+    cpi: "8.63/10",
     year: "Aug 2022 - May 2026",
+    image: IITBImg,
+  },
+  {
+    id: 2,
+    degree: "Senior Secondary",
+    minordegree: "PCM + Computer Science",
+    institution: "Delhi Public School, DPS Bopal",
+
+    year: "Apr 2019 - Mar 2021",
+    image: IITBImg,
+  },
+  {
+    id: 3,
+    degree: "Secondary",
+    minordegree: "Science",
+    institution: "Delhi Public School, DPS Bopal",
+    year: "Apr 2017 - Mar 2019",
     image: IITBImg,
   },
 ];
